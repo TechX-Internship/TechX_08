38fe2ca0ae725cd68cd333e9beb15830e972902e
Deep Kothari <deepk2891@gmail.com>
2023-12-29 00:12:55 +0530

    navigation

---
+++ frontend/src/app/aboutus/page.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+const aboutus = () => {
+	return (
+		<>
+			<Header />
+			<div>aboutus</div>
+			<Footer />
+		</>
+	)
+}
+
+export default aboutus
---
+++ frontend/src/app/productenquiry/page.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+const productenquiry = () => {
+	return (
+		<>
+			<Header />
+			<div>productenquiry</div>
+			<Footer />
+		</>
+	)
+}
+
+export default productenquiry
