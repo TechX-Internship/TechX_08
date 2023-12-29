6e0beba09dda263f7c8dca47e54a52e0ae6adabb
Deep Kothari <deepk2891@gmail.com>
2023-12-29 23:56:48 +0530

    created mainbanner -pending

--- frontend/src/app/Home/page.jsx
+++ frontend/src/app/Home/page.jsx
@@ -4,18 +4,25 @@ import styles from "../styles/home.module.css"
 
 const HomePage = () => {
 	return (
-		<main className={`${styles.main} container-fluid`}>
-			<div className={styles.container}>
-				<div className={styles.data}>
-					<h2>
-						We Have <span className={styles.years}> 10 Years</span>
-						<br /> Of LeadershipIn <br /> Bopp And Polyster Films.
-					</h2>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
-					<button>Get a Quote</button>
-				</div>
-			</div>
-		</main>
+		<>
+			<main className={styles.main}>
+				<section className={`${styles.mainbanner} `}>
+					<div className="container-fluid p-0">
+						<div className={styles.leftdata}>
+							<div className="">
+								<h2>
+									We Have <span>10 Years</span>
+									<br /> Of LeadershipIn Bopp And
+									<br /> Polyster Films.
+								</h2>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+								<button>Get a quote</button>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
+		</>
 	)
 }
 
